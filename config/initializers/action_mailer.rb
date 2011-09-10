f4379d64c3b510a49f9863297e4180d873c993ee
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-10 20:40:35 +0100

    Added the initializer to configure the "Action Mailer" to send and receive emails.

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,8 @@
+# http://guides.rubyonrails.org/v2.3.11/action_mailer_basics.html
+CONFIG['action_mailer']['delivery_method'] = CONFIG['action_mailer']['delivery_method'].to_sym
+CONFIG['action_mailer']['smtp_settings']['authentication'] = CONFIG['action_mailer']['smtp_settings']['authentication'].to_sym
+
+ActionMailer::Base.raise_delivery_errors = CONFIG['action_mailer']['raise_delivery_errors']
+ActionMailer::Base.delivery_method = CONFIG['action_mailer']['delivery_method']
+ActionMailer::Base.smtp_settings = CONFIG['action_mailer']['smtp_settings']
+ActionMailer::Base.default_url_options = CONFIG['action_mailer']['default_url_options']
