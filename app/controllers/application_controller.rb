64b3e305724bcd9c1b3121898aa06c9959bce381
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-10 21:06:00 +0100

    Included the "Exception Notification" plugin to the application to receive emails when something goes wrong.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
+  # With this line we should receive some emails when something goes wrong.
+  # TODO : Test if we need to put this line starting the class. Will the errors on the helpers cached ?
+  include ExceptionNotification::Notifiable
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
