c8a9ca3637733bd0615300e027e577da95d6c5aa
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-19 02:30:23 +0100

    Changed the "engine" and the "charset" of the table, added the "created_by" and "updated_by" fields, and added constraints for the two (2) new fields.

--- db/migrate/20110917002750_devise_create_users.rb
+++ db/migrate/20110917002750_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
-    create_table(:users) do |t|
+    create_table( :users, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' ) do |t|
       t.column :username, :string, :null => false, :default => ''
       t.database_authenticatable :null => false
       t.confirmable
@@ -8,15 +8,29 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.rememberable
       t.trackable
       # t.lockable
+      t.column :created_by, :int, :null => false
+      t.column :updated_by, :int, :null => false
 
       t.timestamps
     end
 
+    # Add the index
     add_index :users, :username,      :unique => true
     add_index :users, :email,                :unique => true
     add_index :users, :confirmation_token,   :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :unlock_token,         :unique => true
+    
+    # Add the foreign keys
+    execute <<-SQL
+      ALTER TABLE `users`
+        ADD CONSTRAINT `fk_users_on_created_by`
+        FOREIGN KEY ( `created_by` )
+        REFERENCES `users`( `id` ),
+        ADD CONSTRAINT `fk_users_on_updated_by`
+        FOREIGN KEY ( `updated_by` )
+        REFERENCES `users`( id )
+    SQL
   end
 
   def self.down
