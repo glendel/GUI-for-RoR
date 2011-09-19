bb7230475f1f44bac1da9ce943e4d2bcd876ec8f
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-19 02:38:29 +0100

    - Moved the "include" to the beginning of the class. - Added a method to "load_and_set_settings" for the application. - Uncommented the "check_authorization" to ensure the authorization. - Filtered the "password" and "password_confirmation" from the logs. - Added two (2) exceptions handlers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,34 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  # With this line we should receive emails with useful information to determine the errors when something goes wrong.
+  include ExceptionNotification::Notifiable
+  
   # With this line we ensure that only an authenticated user will be able to use the application.
   before_filter :authenticate_user!
   
+  # This method will be invoked to load and set convenient data for the general application.
+  before_filter :load_and_set_settings
+  
   # With this line we ensure that the authorization happens on every action in the application.
-  #check_authorization
+  check_authorization
   
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
-  # With this line we should receive some emails when something goes wrong.
-  # TODO : Test if we need to put this line starting the class. Will the errors on the helpers cached ?
-  include ExceptionNotification::Notifiable
-  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
+  
+  # Exceptions handlers
+  #rescue_from Exception, :with => :rescue_from_exception
+  #rescue_from CanCan::AccessDenied, :with => :rescue_from_cancan
+  
+  protected
+    #----------------------------------------------------------------------------------------------------
+    # load_and_set_settings
+    #----------------------------------------------------------------------------------------------------
+    def load_and_set_settings
+      
+    end
 end
