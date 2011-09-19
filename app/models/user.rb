cb734a8f883a58d15073a4d2eed67e78b6ad60ca
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-19 02:24:24 +0100

    - Added the "username", the "created_by", and the "updated_by" fields, and created the association with the roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
   devise :registerable, :database_authenticatable, :recoverable,
-         :rememberable, :trackable, :validatable
-
+             :rememberable, :trackable, :validatable
+  
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :created_by, :updated_by
+  
+  # Associations
+  has_and_belongs_to_many :roles, :join_table => 'user_roles'
 end
