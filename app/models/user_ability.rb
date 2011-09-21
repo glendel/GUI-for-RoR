f3c35b27cd590424c21f67ae2711e241be40bccd
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-21 02:57:01 +0100

    Filtered the "user" variable to work only with instances of the "User" model.

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -2,8 +2,10 @@ class UserAbility
   include CanCan::Ability
   
   def initialize( user )
-    if ( user.is?( :Admin ) )
-      can :manage, :all
+    if ( user.instance_of?( User ) )
+      if ( user.is?( :Admin ) )
+        can :manage, :all
+      end
     end
   end
 end
