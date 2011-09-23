e05fa0abbd75d9426dc156612129585748ccdd74
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-23 19:55:30 +0100

    Created the migration file for the association table between "menus" and "menu_items".

---
+++ db/migrate/20110923154031_create_menu_items_menus.rb
@@ -0,0 +1,41 @@
+class CreateMenuItemsMenus < ActiveRecord::Migration
+  def self.up
+    create_table( :menu_items_menus, :id => false, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' ) do |t|
+      t.column :foreign_table_id, :int, :null => false
+      t.references :menu_item
+      t.column :foreign_table_type, :string, :limit => 10, :null => false, :default => 'menu', :comment => 'Just supported menu or menu_item as foreign tables'
+      t.column :created_by, :int, :null => false
+      t.column :updated_by, :int, :null => false
+
+      t.timestamps
+    end
+    
+    # Add the foreign keys
+    execute <<-SQL
+      SET FOREIGN_KEY_CHECKS = 0;
+    SQL
+    execute <<-SQL
+      ALTER TABLE `menu_items_menus`
+        ADD CONSTRAINT `index_menu_items_menus_on_foreign_table_id`
+        KEY ( `foreign_table_id` ),
+        ADD CONSTRAINT `fk_menu_items_menus_on_menu_item_id`
+        FOREIGN KEY ( `menu_item_id` )
+        REFERENCES `menu_items`( `id` ),
+	ADD CONSTRAINT `index_menu_items_menus_on_unique_key`
+	UNIQUE KEY ( `foreign_table_id`, `menu_item_id`, `foreign_table_type` ),
+        ADD CONSTRAINT `fk_menu_items_menus_on_created_by`
+        FOREIGN KEY ( `created_by` )
+        REFERENCES `users`( `id` ),
+        ADD CONSTRAINT `fk_menu_items_menus_on_updated_by`
+        FOREIGN KEY ( `updated_by` )
+        REFERENCES `users`( id );
+    SQL
+    execute <<-SQL
+      SET FOREIGN_KEY_CHECKS = 1;
+    SQL
+  end
+  
+  def self.down
+    drop_table :menu_items_menus
+  end
+end
