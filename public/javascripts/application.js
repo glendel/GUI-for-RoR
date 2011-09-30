d5a9231d577e7ce98f7e65fc7ac7f6a225377f2f
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-09-30 06:08:29 +0100

    Added the structure for two (2) functions that will handle the button events.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,28 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Function to handle the mousedown event on the buttons.
+function buttonMouseDownHandler( eventObject ) {
+  if ( eventObject.data.actAsLink ) {
+    window.location.href = eventObject.data.url;
+  } else if ( eventObject.data.isADialog ) {
+    
+  } else {
+    
+  }
+}
+
+// Function to handle the mousedown event on the menu buttons.
+function menuButtonMouseDownHandler( eventObject ) {
+  if ( eventObject.data.hasMenuItems ) {
+    var menuItems = jQuery( '#menu_items_for_menu' + eventObject.data.id );
+    
+    if ( menuItems.length > 0 ) {
+      
+    } else {
+      
+    }
+  } else {
+    buttonMouseDownHandler( eventObject );
+  }
+}
