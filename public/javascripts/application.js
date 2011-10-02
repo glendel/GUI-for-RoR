a20e8c52b861deba2f352d3940b7d1c0754c5c9e
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-10-02 18:56:44 +0100

    Improved the code to handle the "mousedown" event on the buttons.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,22 +6,60 @@ function buttonMouseDownHandler( eventObject ) {
   if ( typeof( eventObject.data.callback ) === 'function' ) {
     eventObject.data.callback( eventObject );
   } else if ( typeof( eventObject.data.url ) === 'string' ) {
-    if ( eventObject.data.isAJAX ) {
-      jQuery.get( eventObject.data.url, jQuery.extend( { isAJAX : true }, eventObject.data.data ), function( data, textStatus, jqXHR ) {
-        jQuery( '#content' ).html( data );
-      } );
-    } else if ( eventObject.data.isADialog ) {
-      var dialog = jQuery( '#dialog' );
-      
-      if ( dialog.length < 1 ) {
-        dialog = jQuery( '<div id="dialog" style="display:none;"></div>' );
-      }
-      
-      dialog.load( eventObject.data.url, jQuery.extend( { isADialog : true }, eventObject.data.data ), function( responseText, textStatus, XMLHttpRequest ) {
-        dialog.dialog( eventObject.data.options );
+    var data = jQuery.extend( { isAJAX : eventObject.data.isAJAX, isADialog : eventObject.data.isADialog }, eventObject.data.data );
+    
+    for ( key in eventObject.data.attributes ) {
+      data[ key ] = this.getAttribute( eventObject.data.attributes[ key ] );
+    }
+    
+    if ( eventObject.data.isAJAX || eventObject.data.isADialog ) {
+      jQuery.ajax( {
+        url : eventObject.data.url,
+        data : data,
+        type : eventObject.data.type,
+        success : function( data, textStatus, jqXHR ) {
+          if ( typeof( eventObject.data.success ) === 'function' ) {
+            eventObject.data.success( data, textStatus, jqXHR );
+          } else {
+            if ( textStatus === 'success' ) {
+              if ( eventObject.data.isAJAX ) {
+                var selector = ( typeof( eventObject.data.selector ) === 'string' ) ? eventObject.data.selector : '#content';
+                
+                jQuery( selector ).html( data );
+              } else { // eventObject.data.isADialog
+                var dialog = jQuery( '#dialog' );
+                
+                if ( dialog.length < 1 ) {
+                  dialog = jQuery( '<div id="dialog" style="display:none;"></div>' );
+                }
+                
+                dialog.html( data ).dialog( eventObject.data.options );
+              }
+            }
+          }
+        },
+        error : function( jqXHR, textStatus, errorThrown ) {
+          if ( typeof( eventObject.data.error ) === 'function' ) {
+            eventObject.data.error( jqXHR, textStatus, errorThrown );
+          } else {
+            var dialog = jQuery( '#dialog' );
+            
+            if ( dialog.length < 1 ) {
+              dialog = jQuery( '<div id="dialog" style="display:none;"></div>' );
+            }
+            
+            dialog.html( errorThrown ).dialog( {
+              title : 'Error',
+              resizable : false
+            } );
+          }
+        }
       } );
     } else { // eventObject.data.actAsLink
-      window.location.href = eventObject.data.url;
+      var rquery = /\?/;
+      var params = ( rquery.test( eventObject.data.url ) ? '&' : '?' ) + jQuery.param( data, jQuery.ajaxSettings.traditional );
+      
+      window.location.href = eventObject.data.url + params;
     }
   }
 }
