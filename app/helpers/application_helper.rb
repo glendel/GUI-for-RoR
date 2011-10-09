2509c919e8bb1c91ffd466afa53dcea1c463316b
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-10-09 18:55:36 +0100

    Added the "set_page_title" method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,23 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #----------------------------------------------------------------------------------------------------
+  # csrf_meta_tag
+  #----------------------------------------------------------------------------------------------------
   def csrf_meta_tag
     if ( protect_against_forgery? )
       out = %( <meta name="csrf-param" content="%s"/>\n )
       out << %( <meta name="csrf-token" content="%s"/> )
       out % [ Rack::Utils.escape_html( request_forgery_protection_token ),
                   Rack::Utils.escape_html( form_authenticity_token ) ]
+    end
+  end
+  
+  #----------------------------------------------------------------------------------------------------
+  # set_page_title
+  #----------------------------------------------------------------------------------------------------
+  def set_page_title( title )
+    content_for( :title ) do
+      title
     end
   end
 end
