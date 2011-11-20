4f7d3ea1a75049b86a6a5cec29803fea4315fbf3
Glendel J. Fyne <glendeljfyne@gmail.com>
2011-11-20 23:46:59 +0000

    Added the "get_url" method.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,21 @@
 class Menu < ActiveRecord::Base
   has_many :menu_items_menus, :as => :foreign_table
   has_many :menu_items, :through => :menu_items_menus
+  
+  #----------------------------------------------------------------------------------------------------
+  # get_url
+  #----------------------------------------------------------------------------------------------------
+  def get_url
+    url = ''
+    
+    if ( self.menu_items.count( 'id' ) > 0 )
+      url = "/menu_items?menu_id=#{self.id}"
+    else
+      url = "/#{self.controller}"
+      url += "/#{self.action}" unless ( self.action.blank? )
+      url += "?#{self.parameters}" unless ( self.parameters.blank? )
+    end
+    
+    return( url )
+  end
 end
