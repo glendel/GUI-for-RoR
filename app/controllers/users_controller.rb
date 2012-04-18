72d725ec7d69480f9cd59823674a3e563ed271c7
Glendel J. Fyne <glendeljfyne@gmail.com>
2012-04-18 05:25:13 +0100

    Tidied up this controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users.xml
   def list
     @users = User.all
-
+    
     respond_to do |format|
       format.html {
         if ( request.xhr? )
@@ -20,15 +20,15 @@ class UsersController < ApplicationController
           render( { :action => 'list' } ) # list.html.erb
         end
       }
-      format.xml  { render :xml => @users }
+      format.xml  { render( { :xml => @users } ) }
     end
   end
-
+  
   # GET /users/1
   # GET /users/1.xml
   def show
     @user = User.find( params[:id] )
-
+    
     respond_to do |format|
       format.html {
         if ( request.xhr? )
@@ -37,15 +37,15 @@ class UsersController < ApplicationController
           render( { :action => 'show' } ) # show.html.erb
         end
       }
-      format.xml  { render :xml => @user }
+      format.xml  { render( { :xml => @user } ) }
     end
   end
-
+  
   # GET /users/new
   # GET /users/new.xml
   def new
     @user = User.new
-
+    
     respond_to do |format|
       format.html {
         if ( request.xhr? )
@@ -54,10 +54,10 @@ class UsersController < ApplicationController
           render( { :action => 'new' } ) # new.html.erb
         end
       }
-      format.xml  { render :xml => @user }
+      format.xml  { render( { :xml => @user } ) }
     end
   end
-
+  
   # GET /users/1/edit
   def edit
     @user = User.find( params[:id] )
@@ -91,44 +91,44 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.xml
   def create
-    @user = User.new(params[:user])
-
+    @user = User.new( params[:user] )
+    
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
+      if ( @user.save )
+        format.html { redirect_to( @user, { :notice => 'User was successfully created.' } ) }
+        format.xml  { render( { :xml => @user, :status => :created, :location => @user } ) }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.html { render( { :action => 'new' } ) }
+        format.xml  { render( { :xml => @user.errors, :status => :unprocessable_entity } ) }
       end
     end
   end
-
+  
   # PUT /users/1
   # PUT /users/1.xml
   def update
-    @user = User.find(params[:id])
-
+    @user = User.find( params[:id] )
+    
     respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.xml  { head :ok }
+      if ( @user.update_attributes( params[:user] ) )
+        format.html { redirect_to( @user, { :notice => 'User was successfully updated.' } ) }
+        format.xml  { head( :ok ) }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.html { render( { :action => 'edit' } ) }
+        format.xml  { render( { :xml => @user.errors, :status => :unprocessable_entity } ) }
       end
     end
   end
-
+  
   # DELETE /users/1
   # DELETE /users/1.xml
   def destroy
-    @user = User.find(params[:id])
+    @user = User.find( params[:id] )
     @user.destroy
-
+    
     respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
+      format.html { redirect_to( users_url ) }
+      format.xml  { head( :ok ) }
     end
   end
 end
