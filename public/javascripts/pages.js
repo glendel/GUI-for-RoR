e840a655e0148631f11c468041a864e620523c1c
Glendel J. Fyne <glendeljfyne@gmail.com>
2012-04-23 02:32:02 +0100

    Added the source to handle tabs on the pages like the "home" page.

---
+++ public/javascripts/pages.js
@@ -0,0 +1,11 @@
+/**
+ * Global Variables
+ **/
+var pagesTabsSelector = '#pages_tabs';
+
+/**
+ * pagesTabsHandler
+ **/
+function pagesTabsHandler( $, options ) {
+  $( pagesTabsSelector ).tabs( options );
+}
